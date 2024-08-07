db73151f58b6dd433b7ffac312388f38515da30a
Benedict Paul <benedict1426paul@gmail.com>
2024-08-07 20:15:46 +0530

    IntegerToEnglishWords_273

---
+++ LeetCode/DailyQuestion/IntegerToEnglishWords_273.java
@@ -0,0 +1,54 @@
+package DailyQuestion;
+import java.util.*;
+
+//Time Complexity - O(Log(N))
+public class IntegerToEnglishWords_273 {
+	private static final String[] belowTwenty = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private static final String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+    public static String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+        return helper(num);
+    }
+
+    private static  String helper(int num) {
+        StringBuilder result = new StringBuilder();
+        if (num < 20) {
+            result.append(belowTwenty[num]);
+        } else if (num < 100) {
+            result.append(tens[num / 10]).append(" ").append(belowTwenty[num % 10]);
+        } else if (num < 1000) {
+            result.append(helper(num / 100)).append(" Hundred ").append(helper(num % 100));
+        } else if (num < 1000000) {
+            result.append(helper(num / 1000)).append(" Thousand ").append(helper(num % 1000));
+        } else if (num < 1000000000) {
+            result.append(helper(num / 1000000)).append(" Million ").append(helper(num % 1000000));
+        } else {
+            result.append(helper(num / 1000000000)).append(" Billion ").append(helper(num % 1000000000));
+        }
+        return result.toString().trim();
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num =sc.nextInt();
+		System.out.println(numberToWords(num));
+	}
+}
+/*
+Example 1:
+
+Input: num = 123
+Output: "One Hundred Twenty Three"
+Example 2:
+
+Input: num = 12345
+Output: "Twelve Thousand Three Hundred Forty Five"
+Example 3:
+
+Input: num = 1234567
+Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+ 
+*/
--- LeetCode/README.md
+++ LeetCode/README.md
@@ -11,6 +11,7 @@ Feel free to fork this repository and contribute by adding more problems or impr
 | ID   | Title | Tags | Difficulty | Solved | Solution File |
 |------|-------|------|------------|--------|----------------|
 | 2053 | [Kth Distinct String in an Array](https://leetcode.com/problems/kth-distinct-string-in-an-array/) | Array, Hash Table, String, Counting | Easy | ✔️ | [Solution for Question 2053](https://github.com/benduBytes/Competitive-Programming/blob/main/LeetCode/DailyQuestion/kthDistinct_2053.java) |
+| 2073 | [Integer to English Words](https://leetcode.com/problems/integer-to-english-words/) | Math, String, Recursion | Hard | ✔️ | [Solution for Question 2073](https://github.com/benduBytes/Competitive-Programming/blob/main/LeetCode/DailyQuestion/IntegerToEnglishWords_273) |
 | 3014 | [Minimum Number of Pushes to Type Word I](https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i/) | Math, String, Greedy | Easy | ✔️ | [Solution for Question 3014](https://github.com/benduBytes/Competitive-Programming/blob/main/LeetCode/LeetCode_Problemset/MinimumNumberOfPushesToTypeWord1_3014.java) |
 | 3016 | [Minimum Number of Pushes to Type Word II](https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/) | Hash Table, String, Greedy, Sorting, Counting | Medium | ✔️ | [Solution for Question 3016](https://github.com/benduBytes/Competitive-Programming/blob/main/LeetCode/DailyQuestion/MinimumNumberOfPushesToTypeWord2_3016.java) |
 
