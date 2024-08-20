092690d76b922c637146452f0668d74306db6b84
Benedict Paul <benedict1426paul@gmail.com>
2024-08-20 23:41:05 +0530

    StoneGame2_1140.java

---
+++ LeetCode/DailyQuestion/StoneGame2_1140.java
@@ -0,0 +1,41 @@
+package DailyQuestion;
+
+//Time complexity is O(N^3) 
+public class StoneGame2_1140 {
+	public int stoneGameII(int[] piles) {
+        int n = piles.length;
+        
+        int[][] dp = new int[n][n + 1];
+        int[] suffixSum = new int[n];
+        suffixSum[n - 1] = piles[n - 1];
+        
+        for (int i = n - 2; i >= 0; i--) {
+            suffixSum[i] = suffixSum[i + 1] + piles[i];
+        }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int m = 1; m <= n; m++) {
+                if (i + 2 * m >= n) {
+                    dp[i][m] = suffixSum[i];
+                } else {
+                    for (int x = 1; x <= 2 * m; x++) {
+                        dp[i][m] = Math.max(dp[i][m], suffixSum[i] - dp[i + x][Math.max(m, x)]);
+                    }
+                }
+            }
+        }
+        
+        return dp[0][1];
+    }
+}
+
+/*
+Example 1:
+Input: piles = [2,7,9,4,4]
+Output: 10
+Explanation:  If Alice takes one pile at the beginning, Bob takes two piles, then Alice takes 2 piles again. Alice can get 2 + 4 + 4 = 10 piles in total. If Alice takes two piles at the beginning, then Bob can take all three piles left. In this case, Alice get 2 + 7 = 9 piles in total. So we return 10 since it's larger. 
+
+Example 2:
+Input: piles = [1,2,3,4,5,100]
+Output: 104
+*/
