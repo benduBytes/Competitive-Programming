04de032b11d0f28e49a41f8cfc3fd0823e302105
Benedict Paul <benedict1426paul@gmail.com>
2024-08-21 23:32:01 +0530

    StrangePrinter_664.java

---
+++ LeetCode/DailyQuestion/StrangePrinter_664.java
@@ -0,0 +1,46 @@
+package DailyQuestion;
+import java.util.*;
+
+//Time complexity is O(N^3) 
+public class StrangePrinter_664 {
+	public int strangePrinter(String s) {
+        int n = s.length();
+        char[] sChar = s.toCharArray();
+        int[][] dp = new int[n][n];
+        for(int[] in : dp) Arrays.fill(in, -1);
+        return Util(0, n - 1, sChar, dp);
+    }
+    public int Util(int i, int j, char[] sChar, int[][] dp) {
+        if (i > j) {
+            return 0;
+        }
+
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int firstLetter = sChar[i];
+        // in case, current character is not repeated in the rest of the string
+        int answer = 1 + Util(i + 1, j, sChar, dp);
+        for (int k = i + 1; k <= j; k++) {
+            // if repeated then update the answer
+            if (sChar[k] == firstLetter) {   
+                // splitting from i -> k - 1(remove the last character)
+                // and from k + 1 -> j             
+                int betterAnswer = Util(i, k - 1, sChar, dp) + Util(k + 1, j, sChar, dp);
+                answer = Math.min(answer, betterAnswer);
+            }
+        }
+        return dp[i][j] = answer;
+    }
+}
+
+/*
+Example 1:
+Input: s = "aaabbb"
+Output: 2
+Explanation: Print "aaa" first and then print "bbb".
+
+Example 2:
+Input: s = "aba"
+Output: 2
+Explanation: Print "aaa" first and then print "b" from the second place of the string, which will cover the existing character 'a'.
+*/
