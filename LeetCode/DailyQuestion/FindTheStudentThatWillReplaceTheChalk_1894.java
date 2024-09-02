ad1cb0fe681525d8f67b7829cf34f86fa56eb2c0
Benedict Paul <benedict1426paul@gmail.com>
2024-09-02 21:24:28 +0530

    FindTheStudentThatWillReplaceTheChalk_1894

---
+++ LeetCode/DailyQuestion/FindTheStudentThatWillReplaceTheChalk_1894.java
@@ -0,0 +1,47 @@
+package DailyQuestion;
+
+//Time Complexity - O(N)
+
+public class FindTheStudentThatWillReplaceTheChalk_1894 {
+	public int chalkReplacer(int[] chalk, int k) {
+        long accSum = 0;
+        for (int c : chalk) accSum += c;
+
+        k %= accSum;
+
+        for (int i = 0; i < chalk.length; i++) {
+            if (chalk[i] > k) return i;
+            k -= chalk[i];
+        }
+        return -1; 
+    }
+}
+
+/*
+Example 1:
+Input: chalk = [5,1,5], k = 22
+Output: 0
+Explanation: The students go in turns as follows:
+- Student number 0 uses 5 chalk, so k = 17.
+- Student number 1 uses 1 chalk, so k = 16.
+- Student number 2 uses 5 chalk, so k = 11.
+- Student number 0 uses 5 chalk, so k = 6.
+- Student number 1 uses 1 chalk, so k = 5.
+- Student number 2 uses 5 chalk, so k = 0.
+Student number 0 does not have enough chalk, so they will have to replace it.
+
+Example 2:
+Input: chalk = [3,4,1,2], k = 25
+Output: 1
+Explanation: The students go in turns as follows:
+- Student number 0 uses 3 chalk so k = 22.
+- Student number 1 uses 4 chalk so k = 18.
+- Student number 2 uses 1 chalk so k = 17.
+- Student number 3 uses 2 chalk so k = 15.
+- Student number 0 uses 3 chalk so k = 12.
+- Student number 1 uses 4 chalk so k = 8.
+- Student number 2 uses 1 chalk so k = 7.
+- Student number 3 uses 2 chalk so k = 5.
+- Student number 0 uses 3 chalk so k = 2.
+Student number 1 does not have enough chalk, so they will have to replace it.
+*/
