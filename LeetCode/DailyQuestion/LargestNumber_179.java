b4802d145e2ee1de3cb7afd6536b09d3c82884bb
Benedict Paul <benedict1426paul@gmail.com>
2024-09-18 20:58:26 +0530

    LargestNumber_179.java

---
+++ LeetCode/DailyQuestion/LargestNumber_179.java
@@ -0,0 +1,34 @@
+package DailyQuestion;
+
+import java.util.Arrays;
+
+//Time Complexity: O(NLogN)
+//Space Complexity: O(N)
+
+public class LargestNumber_179 {
+	public String largestNumber(int[] nums) {
+        String[] array =  new String[nums.length];
+        for(int i=0; i<nums.length; i++){
+            array[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(array,(a,b)-> (b+a).compareTo(a+b));
+        if(array[0].equals("0")){
+            return "0";
+        }
+        StringBuilder largest = new StringBuilder();
+        for(int i=0; i<array.length; i++){
+            largest.append(array[i]);
+        }
+        return largest.toString();
+    }
+}
+
+/*
+Example 1:
+Input: nums = [10,2]
+Output: "210"
+
+Example 2:
+Input: nums = [3,30,34,5,9]
+Output: "9534330"
+ */
