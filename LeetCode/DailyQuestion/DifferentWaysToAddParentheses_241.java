272efaaec587b2e732bd78b7ae565d8a9ad5353e
Benedict Paul <benedict1426paul@gmail.com>
2024-09-19 22:07:51 +0530

    DifferentWaysToAddParentheses_241.java

---
+++ LeetCode/DailyQuestion/DifferentWaysToAddParentheses_241.java
@@ -0,0 +1,58 @@
+package DailyQuestion;
+import java.util.*;
+
+//Time Complexity: O(3^N)
+//Space Complexity: O(N^2)
+
+public class DifferentWaysToAddParentheses_241 {
+	public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> ret = new LinkedList<Integer>();
+        for (int i=0; i<input.length(); i++) {
+            if (input.charAt(i) == '-' ||
+                input.charAt(i) == '*' ||
+                input.charAt(i) == '+' ) {
+                String part1 = input.substring(0, i);
+                String part2 = input.substring(i+1);
+                List<Integer> part1Ret = diffWaysToCompute(part1);
+                List<Integer> part2Ret = diffWaysToCompute(part2);
+                for (Integer p1 :   part1Ret) {
+                    for (Integer p2 :   part2Ret) {
+                        int c = 0;
+                        switch (input.charAt(i)) {
+                            case '+': c = p1+p2;
+                                break;
+                            case '-': c = p1-p2;
+                                break;
+                            case '*': c = p1*p2;
+                                break;
+                        }
+                        ret.add(c);
+                    }
+                }
+            }
+        }
+        if (ret.size() == 0) {
+            ret.add(Integer.valueOf(input));
+        }
+        return ret;
+    }
+}
+
+/*
+Example 1:
+Input: expression = "2-1-1"
+Output: [0,2]
+Explanation:
+((2-1)-1) = 0 
+(2-(1-1)) = 2
+
+Example 2:
+Input: expression = "2*3-4*5"
+Output: [-34,-14,-10,-10,10]
+Explanation:
+(2*(3-(4*5))) = -34 
+((2*3)-(4*5)) = -14 
+((2*(3-4))*5) = -10 
+(2*((3-4)*5)) = -10 
+(((2*3)-4)*5) = 10
+*/
