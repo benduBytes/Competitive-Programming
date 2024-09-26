038246f3eb06a9003df602b1cdc6a2e54451fc67
Benedict Paul <benedict1426paul@gmail.com>
2024-09-26 23:07:56 +0530

    MyCalendarI_729.java

---
+++ LeetCode/DailyQuestion/MyCalendarI_729.java
@@ -0,0 +1,32 @@
+package DailyQuestion;
+import java.util.*;
+
+//Time Complexity: O(N * M)
+//Space Complexity: O(N * M)
+
+public class MyCalendarI_729 {
+	TreeMap<Integer,Integer> calendar = new TreeMap<>();
+    public MyCalendarI_729() {
+        calendar.put(Integer.MAX_VALUE, Integer.MAX_VALUE);
+    }
+    public boolean book(int start, int end) {
+        Map.Entry<Integer,Integer> pair = calendar.higherEntry(start);
+        boolean res = end <= pair.getValue();
+        if (res) calendar.put(end, start);
+        return res;
+    }
+}
+
+/*
+Example 1:
+Input 
+["MyCalendar", "book", "book", "book"]
+[[], [10, 20], [15, 25], [20, 30]]
+Output
+[null, true, false, true]
+Explanation
+MyCalendar myCalendar = new MyCalendar();
+myCalendar.book(10, 20); // return True
+myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+*/
