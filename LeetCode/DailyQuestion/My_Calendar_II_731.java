d1f22461d29dd88680f30d21a9a9b1eb721b56ff
Benedict Paul <benedict1426paul@gmail.com>
2024-09-28 00:55:19 +0530

    My_Calendar_II_731.java

---
+++ LeetCode/DailyQuestion/My_Calendar_II_731.java
@@ -0,0 +1,85 @@
+package DailyQuestion;
+import java.util.*;
+
+//Time Complexity: O(N^2)
+//Space Complexity: O(N)
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
+
+class Event
+{
+  int start;
+  int end;
+  public Event(int start, int end)
+  {
+    this.start = start;
+    this.end = end;
+  }  
+}
+
+public class My_Calendar_II_731 {
+	List<Event> overLapBookings;
+    List<Event> bookings;
+
+    public My_Calendar_II_731() {
+        overLapBookings = new ArrayList<>();
+        bookings = new ArrayList<>();
+    }
+    
+    public boolean book(int start, int end) {
+        
+    for(Event e : overLapBookings)
+    {
+      if(isOverlap(e, start, end))
+      return false;  
+    } 
+
+    for(Event e : bookings)
+    {
+      if(isOverlap(e, start, end))
+      {
+        overLapBookings.add(getOverlapEvent(e, start, end));
+      }    
+    }  
+       bookings.add(new Event(start, end));
+
+       return true; 
+    }
+
+    public boolean isOverlap(Event e, int start, int end)
+    {
+      return Math.max(e.start, start)<Math.min(e.end, end); 
+    }
+
+    public Event getOverlapEvent(Event e, int start, int end)
+    {
+      return new Event(Math.max(e.start, start), Math.min(e.end, end));  
+    }
+}
+
+/*
+Example 1:
+Input
+["MyCalendarTwo", "book", "book", "book", "book", "book", "book"]
+[[], [10, 20], [50, 60], [10, 40], [5, 15], [5, 10], [25, 55]]
+Output
+[null, true, true, true, false, true, true]
+Explanation
+MyCalendarTwo myCalendarTwo = new MyCalendarTwo();
+myCalendarTwo.book(10, 20); // return True, The event can be booked. 
+myCalendarTwo.book(50, 60); // return True, The event can be booked. 
+myCalendarTwo.book(10, 40); // return True, The event can be double booked. 
+myCalendarTwo.book(5, 15);  // return False, The event cannot be booked, because it would result in a triple booking.
+myCalendarTwo.book(5, 10); // return True, The event can be booked, as it does not use time 10 which is already double booked.
+myCalendarTwo.book(25, 55); // return True, The event can be booked, as the time in [25, 40) will be double booked with the third event, the time [40, 50) will be single booked, and the time [50, 55) will be double booked with the second event.
+*/
