7b2afc784d71bf17c4e84581b39681c3fdca61db
Benedict Paul <benedict1426paul@gmail.com>
2024-10-01 23:27:39 +0530

    CheckIfArrayPairsAreDivisibleByK_1497.java

---
+++ LeetCode/DailyQuestion/CheckIfArrayPairsAreDivisibleByK_1497.java
@@ -0,0 +1,38 @@
+package DailyQuestion;
+
+//Time Complexity: O(N)
+//Space Complexity: O(K)
+
+public class CheckIfArrayPairsAreDivisibleByK_1497 {
+	public boolean canArrange(int[] arr, int k) {
+        int[] frequency = new int[k];
+        for(int num : arr){
+            num %= k;
+            if(num < 0) num += k;
+            frequency[num]++;
+        }
+        if(frequency[0]%2 != 0) return false;
+        
+        for(int i = 1; i <= k/2; i++)
+            if(frequency[i] != frequency[k-i]) return false;
+			
+        return true;
+    }
+}
+
+/*
+Example 1:
+Input: arr = [1,2,3,4,5,10,6,7,8,9], k = 5
+Output: true
+Explanation: Pairs are (1,9),(2,8),(3,7),(4,6) and (5,10).
+
+Example 2:
+Input: arr = [1,2,3,4,5,6], k = 7
+Output: true
+Explanation: Pairs are (1,6),(2,5) and(3,4).
+
+Example 3:
+Input: arr = [1,2,3,4,5,6], k = 10
+Output: false
+Explanation: You can try all possible pairs to see that there is no way to divide arr into 3 pairs each with sum divisible by 10.
+*/
