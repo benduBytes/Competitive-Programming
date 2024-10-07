66f4c48537527ab0610a7fda11e274dab3dc9b0f
Benedict Paul <benedict1426paul@gmail.com>
2024-10-07 19:38:45 +0530

    MinimumStringLengthAfterRemovingSubstrings_2696.java

--- LeetCode/DailyQuestion/MinimumStringLengthAfterRemovingSubstrings_2696.java
+++ LeetCode/DailyQuestion/MinimumStringLengthAfterRemovingSubstrings_2696.java
@@ -1,5 +1,48 @@
 package DailyQuestion;
+import java.util.*;
+
+//Time Complexity: O(N)
+//Space Complexity: O(N)
 
 public class MinimumStringLengthAfterRemovingSubstrings_2696 {
+	public int minLength(String s) {
+        Stack<Character> stack = new Stack<>();
 
+        for (int i = 0; i < s.length(); i++) {
+            char cur_char = s.charAt(i);
+
+            if (stack.isEmpty()) {
+                stack.push(cur_char);
+                continue;
+            }
+      
+            if (cur_char == 'B' && stack.peek() == 'A') {
+                stack.pop();
+            }
+            else if (cur_char == 'D' && stack.peek() == 'C') {
+                stack.pop();
+            }
+            else {
+                stack.push(cur_char);
+            }
+        }
+
+        return stack.size();
+    }
 }
+/*
+Example 1:
+Input: s = "ABFCACDB"
+Output: 2
+Explanation: We can do the following operations:
+- Remove the substring "ABFCACDB", so s = "FCACDB".
+- Remove the substring "FCACDB", so s = "FCAB".
+- Remove the substring "FCAB", so s = "FC".
+So the resulting length of the string is 2.
+It can be shown that it is the minimum length that we can obtain.
+
+Example 2:
+Input: s = "ACBBD"
+Output: 5
+Explanation: We cannot do any operations on the string so the length remains the same.
+*/
