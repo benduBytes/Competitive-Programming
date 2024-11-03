651dba496660ff1b89d428c5c51f67730eaeb0da
Benedict Paul <benedict1426paul@gmail.com>
2024-11-03 20:55:00 +0530

    RotateString_796.java

--- LeetCode/DailyQuestion/RotateString_796.java
+++ LeetCode/DailyQuestion/RotateString_796.java
@@ -1,5 +1,44 @@
 package DailyQuestion;
+import java.util.*;
+
+//Time Complexity: O(N^2)
+//Space Complexity: O(1)
 
 public class RotateString_796 {
-
+	public boolean rotateString(String A, String B) {
+        if(A == null || B == null) {
+            //throw exception on A and B both being null?
+            return false;
+        }
+        if(A.length() != B.length()) {
+            return false;
+        }
+        if(A.length() == 0) {
+            return true;
+        }
+        for(int i = 0; i < A.length(); i++) {
+            if(rotateString(A, B, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean rotateString(String A, String B, int rotation) {
+        for(int i = 0; i < A.length(); i++) {
+            if(A.charAt(i) != B.charAt((i+rotation)%B.length())) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+/*
+Example 1:
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Example 2:
+Input: s = "abcde", goal = "abced"
+Output: false
+*/
