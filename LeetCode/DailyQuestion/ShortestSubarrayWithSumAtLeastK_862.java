7389c8d7fe5ea409aab46e067d028d31c34e9d6d
Benedict Paul <benedict1426paul@gmail.com>
2024-11-17 22:04:15 +0530

    ShortestSubarrayWithSumAtLeastK_862.java

---
+++ LeetCode/DailyQuestion/ShortestSubarrayWithSumAtLeastK_862.java
@@ -0,0 +1,35 @@
+package DailyQuestion;
+import java.util.*;
+
+//Time Complexity: O(N)
+//Space Complexity: O(N)
+
+public class ShortestSubarrayWithSumAtLeastK_862 {
+	public int shortestSubarray(int[] A, int K) {
+        int N = A.length, res = N + 1;
+        long[] B = new long[N + 1];
+        for (int i = 0; i < N; i++) B[i + 1] = B[i] + A[i];
+        Deque<Integer> d = new ArrayDeque<>();
+        for (int i = 0; i < N + 1; i++) {
+            while (d.size() > 0 && B[i] - B[d.getFirst()] >=  K)
+                res = Math.min(res, i - d.pollFirst());
+            while (d.size() > 0 && B[i] <= B[d.getLast()])
+                d.pollLast();
+            d.addLast(i);
+        }
+        return res <= N ? res : -1;
+    }
+}
+/*
+Example 1:
+Input: nums = [1], k = 1
+Output: 1
+
+Example 2:
+Input: nums = [1,2], k = 4
+Output: -1
+
+Example 3:
+Input: nums = [2,-1,2], k = 3
+Output: 3
+*/
