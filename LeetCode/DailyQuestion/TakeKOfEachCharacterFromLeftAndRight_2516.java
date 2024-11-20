d96c0a19ec35b67493e989d97b9f4683816454a3
Benedict Paul <benedict1426paul@gmail.com>
2024-11-20 15:33:55 +0530

    TakeKOfEachCharacterFromLeftAndRight_2516.java

---
+++ LeetCode/DailyQuestion/TakeKOfEachCharacterFromLeftAndRight_2516.java
@@ -0,0 +1,48 @@
+package DailyQuestion;
+import java.util.*;
+
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+
+public class TakeKOfEachCharacterFromLeftAndRight_2516 {
+	public int takeCharacters(String s, int k) {
+        // Total counts
+        int[] count = new int[3];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        
+        if (Math.min(Math.min(count[0], count[1]), count[2]) < k) {
+            return -1;
+        }
+        
+        // Sliding Window
+        int res = Integer.MAX_VALUE;
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            count[s.charAt(r) - 'a']--;
+            
+            while (Math.min(Math.min(count[0], count[1]), count[2]) < k) {
+                count[s.charAt(l) - 'a']++;
+                l++;
+            }
+            res = Math.min(res, s.length() - (r - l + 1));
+        }
+        return res;
+    }
+}
+/*
+Example 1:
+Input: s = "aabaaaacaabc", k = 2
+Output: 8
+Explanation: 
+Take three characters from the left of s. You now have two 'a' characters, and one 'b' character.
+Take five characters from the right of s. You now have four 'a' characters, two 'b' characters, and two 'c' characters.
+A total of 3 + 5 = 8 minutes is needed.
+It can be proven that 8 is the minimum number of minutes needed.
+
+Example 2:
+Input: s = "a", k = 1
+Output: -1
+Explanation: It is not possible to take one 'b' or 'c' so return -1.
+*/
