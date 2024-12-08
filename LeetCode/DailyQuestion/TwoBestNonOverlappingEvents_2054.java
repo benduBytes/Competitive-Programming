3410d20366afc3f1989133108e0e5ffc549a682a
Benedict Paul <benedict1426paul@gmail.com>
2024-12-08 06:51:42 +0530

    TwoBestNonOverlappingEvents_2054.java

---
+++ LeetCode/DailyQuestion/TwoBestNonOverlappingEvents_2054.java
@@ -0,0 +1,52 @@
+package DailyQuestion;
+import java.util.*;
+
+//Time Complexity: O(NLogN)
+//Space Complexity: O(N)
+
+public class TwoBestNonOverlappingEvents_2054 {
+	public int maxTwoEvents(int[][] events) {
+        int dp[][] = new int[events.length][3],output = 0;
+        for(int row[]:dp) Arrays.fill(row,-1);
+        Arrays.sort(events,(a,b)->(a[0]-b[0]));
+        int out =  find(dp,events,0,2);
+        for(int row[]:events) output = Math.max(output,row[2]);
+        return Math.max(out,output);
+    }
+    public int find(int dp[][],int events[][],int n,int count){
+        if(count==0) return 0;
+        if(n>=events.length||n==-1) return Integer.MIN_VALUE;
+        if(dp[n][count]!=-1) return dp[n][count];
+        int op = find(dp,events,n+1,count);
+        int upper = bisearch(events,events[n][1]);
+        int yo = events[n][2]+find(dp,events,upper,count-1);
+        return dp[n][count] = Math.max(op,yo);
+    }
+    public int bisearch(int events[][],int val){
+        int top=0,bot=events.length-1;
+        while(top<bot){
+            int mid = (bot-top)/2+top;
+            if(events[mid][0]>val) bot=mid;
+            else top = mid+1;
+        }
+        if(events[bot][0]<=val) return -1;
+        return bot;
+    }
+}
+/*
+Example 1:
+Input: events = [[1,3,2],[4,5,2],[2,4,3]]
+Output: 4
+Explanation: Choose the green events, 0 and 1 for a sum of 2 + 2 = 4.
+
+Example 2:
+Example 1 Diagram
+Input: events = [[1,3,2],[4,5,2],[1,5,5]]
+Output: 5
+Explanation: Choose event 2 for a sum of 5.
+
+Example 3:
+Input: events = [[1,5,3],[1,5,1],[6,6,5]]
+Output: 8
+Explanation: Choose events 0 and 2 for a sum of 3 + 5 = 8.
+*/
